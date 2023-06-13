1494a38b94679c9f3a35dd155fd7e8942d84a462
chen chen <myxiongwei233@gmail.com>
2023-06-13 22:07:59 +0800

    feat(初次提交): 初次提交

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="app">app</div>
+  <div class="app">
+    <h1 >大家好，我是陈麒麟，我的项目已经初始化完毕</h1>
+    <img src="https://t10.baidu.com/it/fm=173&fmt=auto&h=397&img_JPEG=&s=E63E3EC748912FED9015E1260300F068&u=3931662150%2C1637425949&w=500" alt="">
+    <p>是兄弟就来砍我</p>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.app{
+  text-align: center;
+}
+
+</style>
